9acc62a61d18ac99cd9af7945b8f6320746d454e
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-09-05 16:06:24 +0200

    primi due punti dell'esercizio

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -2,4 +2,18 @@
 
 var colorName = "Blallo";
 
-Console.WriteLine("Ciao, sono una persona molto indecisa, infatti il mio colore preferito è il", colorName, );
+Console.WriteLine("Ciao, sono una persona molto indecisa, infatti il mio cibo preferito è" + colorName );
+
+Console.WriteLine("Ma riguardo il cibo???");
+
+string[] favFood = {"Pizza", "Carbonara", "Tiramisù", "Sushi", "Parmigiana", "Lasagna", "Pallotte Cacio e Ova" };
+
+int foodQuantity = favFood.Length;
+
+Console.WriteLine("Il mio cibo preferito");
+ foreach (string food in favFood)
+{
+    Console.WriteLine(food);
+}
+
+ Console.WriteLine("mi piace veramente tanto cibo, per l'appunto mi piacciono ben" +  foodQuantity + "cibi diversi");
